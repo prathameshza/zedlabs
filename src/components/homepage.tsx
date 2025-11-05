f182ffe66444e93b11ac3f89c0c4f8f452969b98
Prathamesh Zade <prathameshpaware9090@gmail.com>
2025-11-05 20:23:11 +0530

    initial commit

---
+++ src/components/homepage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+    }}>
+      <h1>zedlabs</h1>
+    </div>
+  );
+};
+
+export default HomePage;
